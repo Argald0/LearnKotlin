526a425b0ce899ec6c87a5843f28fb0e61a65b9e
Argald0 <github.account@dimitrijedjokic.com>
2024-02-25 23:56:37 +0100

    enumeration and when

--- app/src/main/java/com/example/learnkotlin/MainActivity.kt
+++ app/src/main/java/com/example/learnkotlin/MainActivity.kt
@@ -90,7 +90,11 @@ class Parent ( var firstname: String = "", var lastname: String = "", var phone:
 class Child (var firstname: String = "", var lastname: String = "", var parent1: Parent, var parent2: Parent, var postalAddress: String = "", var allergies: Array<String> = [])
 */
 
-// Switch cases like in Java
+
+
+
+
+// ##### Switch cases like in Java ########
 
 /*var leRetour = 200
 
@@ -106,9 +110,12 @@ fun main(){
 }*/
 
 
+
+
+
 // ####### Enumeration ######
 
-private enum class ApiResponses(code: Int) {
+/*private enum class ApiResponses(code: Int) {
     OK(200),
     NOT_FOUND(404),
     UNAUTHORIZED(401),
@@ -122,4 +129,117 @@ when (reponse) {
     ApiResponses.OK -> print("OK")
     ApiResponses.FORBIDDEN -> print("FORBIDDEN")
     // ...
+} */
+
+
+
+
+// ###### WHILE ######
+/*const val condition: Boolean = true
+fun main() {
+    while (condition) {
+        println("Condition met.")
+    }
+
+    do {
+        println("Condition met.")
+    } while (condition)
+}
+*/
+
+
+
+
+// ###### Boucle for ######
+/*val nomsComplets = listOf("me", "myself", "I")
+fun main() {
+    for ((index,nom) in nomsComplets.withIndex()) {
+        println("it's $nom on line $index")
+    }
+    //intervalles
+    for (i in 0..9) {
+        print(i)
+        println(" which comes before")
+    }
+
+    // extra for
+    for (j in 10 downTo 1 step 2) println("Index is $j")
+}*/
+
+
+
+
+// ######## List vs Set #########
+/*
+val listOfNames: List<String> = listOf("Jake Wharton", "Joe Birch", "Robert Martin")
+val mutableListOfNames: List<String> = mutableListOf("Jake Wharton", "Joe Birch", "Robert Martin")
+val setOfNames: Set<String> = setOf("Jake Wharton", "Joe Birch", "Robert Martin")
+val mutableSetOfNames: Set<String> = mutableSetOf("Jake Wharton", "Joe Birch", "Robert Martin")
+
+
+fun main(){
+    println("This is mutableListOfNames[0] : $mutableListOfNames[0]")
+    println("Executing : mutableListOfNames[0] = \"Mathieu Nebra\"")
+    mutableListOfNames[0] = "Mathieu Nebra"
+    println(mutableListOfNames)
+    println("Tests on sets \n\n\n")
+    println("mutable set of names : $mutableSetOfNames")
+    println("setOfNames.first() -> " + setOfNames.first())
+    mutableSetOfNames.add("test")
+    println("mutable set of names now : $mutableSetOfNames")
+}*/
+
+
+
+
+// ####### Conversions intelligentes #######
+//Mon idée :
+/*fun getDefaultSize(objetInconnu: Any): Int {
+    if (objetInconnu is String) return objetInconnu.length
+    else if (objetInconnu is List<*>) return objetInconnu.size
+    return 0
+}*/
+
+//Kotlin-friendly
+/*fun getDefaultSize(objetInconnu: Any) = when (objetInconnu) {
+    is String -> objetInconnu.length
+    is List<*> -> objetInconnu.size
+    else -> -1
+}
+
+fun main() {
+    val monObjet = listOf("python", "kotlin", "c++")
+    var resultat = getDefaultSize(monObjet)
+    println(resultat)
+    // Conversion Any -> String
+    // val objetInconnu2: Any = "Slt, c'est moi"
+    // val retour = objetInconnu2 as String
+    // print(retour)
+
+    //not kotlin friendly
+    /*try {
+        val tentative = monObjet as Int
+        println(tentative)
+    }
+    catch (exception: ClassCastException){
+        print ("ClassCastException error !")
+    }*/
+
+    // kotlin friendly
+    val mmessage: Int? = monObjet as? Int
+    print(mmessage)
+}*/
+
+
+
+
+// ######### Exceptions ############
+private fun soustraitNombre(a: Int, b: Int) = if (a >= b) a - b else throw Exception("a is smaller than b!")
+fun main() {
+    try {
+        print(soustraitNombre(5, 6))
+    }
+    catch (e: Exception) {
+        print(e.message)
+    }
 }
